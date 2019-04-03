dc34bcde41630026eb5642a00a789152d3848892
anon <42473928+burrrata@users.noreply.github.com>
2019-04-03 19:10:50 +0000

    _

---
+++ README.md
@@ -0,0 +1,4 @@
+🏆 React app that displays images from APOD
+- 🔥 APOD API: randomly select an image via the API and display it to the screen
+- 🔥 NN Image Transitions: NN that morphs between arbitrary images
+- 🔥 App: a React application that continiously morphs between images from NASA's APOD archives
---
+++ src/App.js
@@ -0,0 +1,61 @@
+// Imports
+//////////////////////////////////////////////////////////
+import React, { Component } from 'react';
+import './App.css';
+
+
+// 🏆 🚫 ✔️ 🔥 ❓ 💭 📍 
+//////////////////////////////////////////////////////////
+
+
+// Params
+//////////////////////////////////////////////////////////
+// This is where global constants go
+
+const canary = '🐤';
+
+
+// Functions
+//////////////////////////////////////////////////////////
+
+// Canary that goes away if there are breaking errors
+function Canary() {
+  return <div className='canary' >{canary}</div>;
+}
+
+class Thing extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    	// stuff goes here
+    }
+  }
+}
+
+
+
+
+
+
+// App 
+//////////////////////////////////////////////////////////
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <div className="app-header">
+					<Canary />
+			    <h1>Hello World</h1>
+				</div>
+				<div className='app-body'>
+          <p>
+						Stuff goes here
+          </p>
+				</div>
+      </div>
+    );
+  }
+}
+
+// Export the app for the rest of the program to use
+export default App;
